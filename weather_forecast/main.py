6bc19e88117f3d0472d7bbc6c31d7557f41d91e2
VijayAnand58 <124174294+VijayAnand58@users.noreply.github.com>
2024-08-28 19:57:35 +0530

    update ports in fast api

--- weather_forecast/main.py
+++ weather_forecast/main.py
@@ -71,8 +71,17 @@ instance=api_transform()
 from typing import Union
 from fastapi import FastAPI,HTTPException,Request
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/data/temp/linear")
 async def get_liner_temp_data():
